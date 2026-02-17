f107439f49dff5447a60407c102d415a6c7da9af
nikolaiv37 <nikolaivanov999@gmail.com>
2026-02-17 10:01:26 +0200

    integration vercel speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { HelmetProvider } from 'react-helmet-async'
@@ -227,6 +228,7 @@ function App() {
             </TenantProvider>
           </BrowserRouter>
           <Toaster />
+          <SpeedInsights />
         </QueryClientProvider>
       </HelmetProvider>
     </ErrorBoundary>
