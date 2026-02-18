0fc7efff61d5a022ff284dc1c0d6c152d6ecf0ee
nikolaiv37 <nikolaivanov999@gmail.com>
2026-02-18 10:04:40 +0200

    change config, now app running

--- vite.config.ts
+++ vite.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    strictPort: true,
+    strictPort: false,
     allowedHosts: ['evromar.local', 'hotfarms.local', 'platform.centivon.local', 'centivon.local'],
   },
 })
